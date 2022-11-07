2b6fb4a38f2b208c0523481583d5c2f9655c8d43
Seif1125 <seifammar1125@gmail.com>
2022-11-07 16:45:55 +0200

    make function to get name from url

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
-<?php
 
-echo 'hello this is first code in php'
-
-
-
+<?php 
+$title='document'  ; 
 ?>
+<html lang="en">
+  
+  <head>
+    
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $title?></title>
+    <style>
+      header {
+        background-color: #e3e3e3;
+        color: #000;
+      }
+    </style>
+  </head>
+  <body>
+  <?php function retname(){ if(isset($_GET['name'])){
+    return htmlspecialchars($_GET['name']);
+  }
+  else{
+    return 'guest';
+  }}
+  ?>
+    <header>
+      <h1><?php echo 'hello  ', retname(); ?></h1>
+    </header>
+  </body>
+</html>
