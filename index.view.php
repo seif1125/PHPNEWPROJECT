3c113cf0af221aaa2cdb80ea616e45b78a6a78f0
Seif1125 <seifammar1125@gmail.com>
2022-11-07 17:04:09 +0200

    seperate view and php

---
+++ index.view.php
@@ -0,0 +1,24 @@
+
+
+<html lang="en">
+  
+  <head>
+    
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $title?></title>
+    <style>
+      header {
+        background-color: #e3e3e3;
+        color: #000;
+      }
+    </style>
+  </head>
+  <body>
+
+    <header>
+      <h1><?php echo 'hello  ', retname(); ?></h1>
+    </header>
+  </body>
+</html>
