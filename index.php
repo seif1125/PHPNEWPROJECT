37e9195188239d70c6a9016e291d0f438c69a292
Seif1125 <seifammar1125@gmail.com>
2022-11-08 14:14:36 +0200

    add lists through array

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
-<?php function retname(){ if(isset($_GET['name'])){
+<?php $title='php';
+$x='';
+function retname(){ if(isset($_GET['name'])){
     return htmlspecialchars($_GET['name']);
   }
   else{
     return 'guest';
-  }}
-  $title='document'  ;
-  $names=['Seif','Mostafa','Ahmed'];
-  $cars=['sunny','xv'];
-  $animals=['rat','cat','dog'];
-   $carsHTML='';
-   foreach ($cars as $car) {
-   echo  '<li>' . $car . '</li>';
-      
-   }
+  }};
+$user=[
+  '0'=>[
+    'user_name'=>'seif'
+  ],
+  '1'=>[
+    'user_name'=>'hazem'
+  ]
+];
+ $tasks=[
+   
+   '0'=> [
+      'task_title' => 'do first thing',
+      'assigned_to'=>'1',
+      'duedate'=>'24/12/2021',
+      'description'=>'we must finish this task as soon as possible ',
+      'completed'=>true,
+   ],
+   
+  
+  ];
+
+
+
    
   require'index.view.php'
   ?>
--- index.view.php
+++ index.view.php
@@ -20,19 +20,20 @@
     <header>
       <h1><?php echo 'hello  ', retname(); ?></h1>
       </header>
-      <ul>
-        <?php foreach($names as $name) :?>
-        <li><?=$name ?></li>
-        <?php endforeach?>
-      </ul>
-      <ul>
-        <?php foreach($animals as $animal) :?>
-         <li> <strong> <?= $animal?></strong></li>
+ 
+      <ul> 
+         
+        <?php foreach($tasks as $key=>$value) :?>
+          
+         <li> <strong> <?= $value['task_title']?></strong></li>
+         <li> <strong> <?= $value['duedate']?></strong></li>
+         <li> <strong> <?= $value['description']?></strong></li>
+         <li> <strong> <?= array_values($user[$value['assigned_to']])[0]?></strong></li>
+         <li style='<?= $value['completed']?'background-color:#00ff00':'background-color:#ff0000'?>'> <strong><?= $value['completed']?'completed':'not-completed'?> </strong></li>
+
           <?php endforeach ?>
       </ul>
-      <ul>
-        <?php $carsHTML ?>
-      </ul>
+  
     
   </body>
 </html>
